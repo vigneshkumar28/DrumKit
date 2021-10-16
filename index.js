6f4d8749bcbac3bc910cbfa9f193ed8f2ab7e06e
VIGNESHKUMAR RADHAKRISHNAN <vigneshkumarponnamaravathy@gmail.com>
2021-10-16 12:35:08 +0530

    cresting audios for all keys

--- index.js
+++ index.js
@@ -4,12 +4,50 @@ for( var i =0; i< noOfDrums; i++) {
 
 document.querySelectorAll(".drum")[i].addEventListener("click" ,function (){
     // alert("i got clicked");
-    var audioPath = 'sounds/tom-'+1+'.mp3';
-    var audio = new Audio(audioPath);
-    audio.play();
+    // this.style.color = 'white';
+    var buttonValue = this.innerHTML;
+    switch (buttonValue) {
+        case 'w':
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case 'a':
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case 's':
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case 'd':
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+        case 'j':
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break; 
+        case 'k':
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+        case 'l':
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;    
+        
+        default:
+            console.log(buttonValue);
+            break;
+    }
 });
 
 }
+
+// var audioPath = 'sounds/tom-'+1+'.mp3';
+//     var audio = new Audio(audioPath);
+//     audio.play();
+
 
 //Higer order function -> function takes another function as argument
 
